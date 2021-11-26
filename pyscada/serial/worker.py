a02e4c73d5fa78461774760fe7310a1f454677ed
clavay <clavayssiere@univ-pau.fr>
2021-11-26 15:23:19 +0100

    fix device with various subdevices

--- pyscada/serial/worker.py
+++ pyscada/serial/worker.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 
 from pyscada.utils.scheduler import SingleDeviceDAQProcessWorker
+from pyscada.serial import PROTOCOL_ID
 
 import logging
 
@@ -12,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 class Process(SingleDeviceDAQProcessWorker):
-    device_filter = dict(serialdevice__isnull=False)
+    device_filter = dict(serialdevice__isnull=False, protocol_id=PROTOCOL_ID)
     bp_label = 'pyscada.serial-%s'
 
     def __init__(self, dt=5, **kwargs):
